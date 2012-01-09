52a36a8499a612cb0ba70a2f7541bcf359f577b0
ks6084 <kohei.sugimoto@mincom.com>
2012-01-09 12:01:44 +0800

    implement setWidgetValue

--- mincom-fitnium/src/com/mincom/fitnium/MincomFitniumFixture.java
+++ mincom-fitnium/src/com/mincom/fitnium/MincomFitniumFixture.java
@@ -1,13 +1,21 @@
 package com.mincom.fitnium;
 
+import com.mincom.ellipse.rc.apiv2.Application;
+
 public class MincomFitniumFixture extends BaseMincomFitniumFixture {
+	
+	private Application application;
 	
 	public MincomFitniumFixture() {
 		super();
 	}
 	
 	public void loadApplication(String app) {
-		mfuiv2.loadApp(app);
+		this.application = mfuiv2.loadApp(app);
+	}
+	
+	public void setWidgetWithValue(String id, String value) {
+		this.application = application.setWidgetValue(id, value);
 	}
 
 }
