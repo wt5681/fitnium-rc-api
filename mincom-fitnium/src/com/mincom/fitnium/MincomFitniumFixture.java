ea84dad84ef50d5c45ab95937de43f17822fbb7b
Woei Boon <woeiboon.tan@mincom.com>
2012-01-10 15:38:53 +0800

    added new fixture

--- mincom-fitnium/src/com/mincom/fitnium/MincomFitniumFixture.java
+++ mincom-fitnium/src/com/mincom/fitnium/MincomFitniumFixture.java
@@ -1,5 +1,6 @@
 package com.mincom.fitnium;
 
+import com.magneticreason.fitnium.api.FitniumVariableAPI;
 import com.mincom.ellipse.rc.apiv2.Application;
 
 public class MincomFitniumFixture extends BaseMincomFitniumFixture {
@@ -11,11 +12,28 @@ public class MincomFitniumFixture extends BaseMincomFitniumFixture {
 	}
 	
 	public void loadApplication(String app) {
-		this.application = mfuiv2.loadApp(app);
+		this.application = mfuiv2.loadApp(app).waitForReady();
 	}
-	
+
 	public void setWidgetWithValue(String id, String value) {
 		this.application = application.setWidgetValue(id, value);
 	}
 
+	public String getWidgetWithValue(String widgetName) {
+		return application.getWidget(widgetName).getValue();
+	}
+	public void selectTheTab(String tabname) {
+		this.application = application.selectTab(tabname).waitForReady();
+	}
+	public void callAnAction(String action ) {
+		this.application = application.toolbarAction(action);
+	}
+	
+	public void waitForLoadedApp() {
+		this.application = application.waitForLoadedApplication();
+	}
+
+	public void confirmAction(String action) {
+		this.application = application.dialogButton(action);
+	}
 }
